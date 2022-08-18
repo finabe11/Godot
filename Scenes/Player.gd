56d97c2c84aa266935aebce89bb98b83d29a984b
finabe11 <finabe11@kingshigh.school.nz>
2022-08-19 10:57:06 +1200

    start door and button

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -55,7 +55,7 @@ func get_input():
 	
 func _physics_process(delta):
 	get_input()
-	print(is_on_floor())
+	"print(is_on_floor())"
 	if velocity == Vector2.ZERO:
 		player_state = state.IDLE
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
