2acb3049bbd7ac58c20c41f448d90db9cab3f9ea
finabe11 <finabe11@kingshigh.school.nz>
2022-09-29 09:53:35 +1300

    if you fall off now you died

---
+++ Scenes/CollisionShape2D.gd
@@ -0,0 +1,13 @@
+extends CollisionShape2D
+
+
+func _ready():
+	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		print("player dead")
+		if GameStats.check_reset() == false:
+			body.global_position = GameStats.get_spawn().global_position 
+
