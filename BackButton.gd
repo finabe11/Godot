6a513005e5cdefe2b01ebdedc1077d1f23f6e3bb
finabe11 <finabe11@kingshigh.school.nz>
2022-11-01 16:14:01 +1300

    doing volume for music and sound effects

--- BackButton.gd
+++ BackButton.gd
@@ -1,9 +1,11 @@
 extends Control
 
+func _on_Back_pressed():
+	get_tree().change_scene("res://NewMenu.tscn")
 
 
-func _ready():
-	pass
+func _on_HSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),value)
 
 
 func _on_Button_pressed():
--- title_screen.gd
+++ title_screen.gd
@@ -7,3 +7,7 @@ func _ready():
 
 func _on_Button_pressed(scene_to_load):
 	get_tree().change_scene(scene_to_load)
+
+
+func _on_ContinueButton_pressed():
+	get_tree().change_scene("res://Continue.tscn")
