042b555f034f928c0fa9fe0fba2ab1e1b2257a44
finabe11 <finabe11@kingshigh.school.nz>
2022-11-02 16:26:47 +1300

    almost done

--- BackButton.gd
+++ BackButton.gd
@@ -1,12 +1,14 @@
 extends Control
 
-func _on_Back_pressed():
-	get_tree().change_scene("res://NewMenu.tscn")
-
-
-func _on_HSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),value)
 
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Title Screen.tscn")
+
+
+func _on_HSlider_value_changed(value):
+	SoundPlayer.change_music_db(value) # Replace with function body.
+
+func _on_HSlider2_value_changed(value):
+	SoundPlayer.change_sound_db(value)
+	
--- Scripts/sound_player.gd
+++ Scripts/sound_player.gd
@@ -1,6 +1,6 @@
 extends Node
 
-onready var music = AudioStreamPlayer.new()
+#onready var music = AudioStreamPlayer.new()
 
 var music_tracks = {
 	"main":"res://Scripts/Y2Mate.is - Lo-fi Type Beat - Rain-ESUTA7utJ6c-160k-1655641202407.mp3",
@@ -11,27 +11,56 @@ var sound_effects = {
 	"hit":"res://Scripts/hitHurt.wav"
 }
 
+#var music_db = 1
+#var sound_db = 1
+
+#func change_music_volume(value):
+	#music_db = linear2db(value)
+	#music_db.volume_db = music_db
+
+
+#func change_sound_volume(value):
+	#sound_db = linear2db(value)
+
+
+#func _ready():
+	#music_player.stream = load(music_tracks["main"])
+	#add_child(music_db)
+	#music_db.play()
+	
+	
+#func play_sound_effect(sfx):
+	#var sound = AudioStreamPlayer.new()
+	#sound.stream = load(sound_effects[sfx])
+	#add_child(sound)
+	#sound.play()
+	#yield(sound, "finished")
+	#sound.queue_free()
+
+
+onready var music = AudioStreamPlayer.new()
+
 var music_db = 1
 var sound_db = 1
 
-func change_music_db(val):
-	music_db = linear2db(val)
+func change_music_db(value):
+	music_db = linear2db(value)
+	music.volume_db = music_db
 
-
-func change_sound_db(val):
-	sound_db = linear2db(val)
+func change_sound_db(value):
+	sound_db = linear2db(value)
 
 
 func _ready():
 	music.stream = load(music_tracks["main"])
 	add_child(music)
 	music.play()
-	
-	
+
 func play_sound_effect(sfx):
 	var sound = AudioStreamPlayer.new()
 	sound.stream = load(sound_effects[sfx])
 	add_child(sound)
+	sound.volume_db = sound_db
 	sound.play()
-	yield(sound, "finished")
+	yield(sound,"finished")
 	sound.queue_free()
