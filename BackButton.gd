c2e00afe53c5e6d160c3b9538984b6a8f2ec4bb6
finabe11 <100319248+finabe11@users.noreply.github.com>
2022-11-03 21:52:27 +1300

    DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- BackButton.gd
+++ BackButton.gd
@@ -6,9 +6,9 @@ func _on_Button_pressed():
 	get_tree().change_scene("res://Title Screen.tscn")
 
 
-func _on_HSlider_value_changed(value):
+func _on_HSlider2_value_changed(value):
 	SoundPlayer.change_music_db(value) # Replace with function body.
 
-func _on_HSlider2_value_changed(value):
+func _on_HSlider_value_changed(value):
 	SoundPlayer.change_sound_db(value)
 	
